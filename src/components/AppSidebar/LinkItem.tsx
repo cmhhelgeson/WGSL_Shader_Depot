1ded9a9f2fc2dd81597425887dfb133b439add41
cmhhelgeson <62450112+cmhhelgeson@users.noreply.github.com>
2023-08-17 12:19:03 -0700

    Fixed nonfunctional redirects

--- src/components/AppSidebar/LinkItem.tsx
+++ src/components/AppSidebar/LinkItem.tsx
@@ -19,6 +19,7 @@ export const LinkItem = ({
   icon,
   isCollapsed,
   mobile,
+  to,
 }: LinkItemProps) => {
   if (!collapsedTitle) {
     collapsedTitle = title;
@@ -64,6 +65,7 @@ export const LinkItem = ({
               }
         }
         onClick={() => {
+          window.location.href = to;
           if (!itemOpen && isCollapsed === true) {
             setItemOpen(true);
           }
